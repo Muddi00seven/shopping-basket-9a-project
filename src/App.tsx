f1401cafc895755347ed953919221a4310fad4fa
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-10-24 02:45:40 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ProductList } from './features/product/ProductList';
-import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import CartItem from "./features/cart/CartItem";
 import Navigation from './Components/header/Navigation'
 
 function App() {
   return (
     <div className="">
-      {/* <Nav /> */}
 <Router>
   <Navigation/>
       <Routes>
-        {/* <Switch> */}
         <Route path="/"> 
           <ProductList />
-          {/* <Footer /> */}
         </Route>
           <Route path="/cart">
             <CartItem />
           </Route>
-        {/* </Switch> */}
         </Routes>
         </Router>
     </div>
